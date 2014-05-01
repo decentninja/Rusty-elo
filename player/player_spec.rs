50d86e565376928aa43af8683fd2f1b0d175e916
Andreas Linn <encrypt.wave@gmail.com>
2014-05-01 21:25:37 +0200

    Stubbing?

--- player/player_spec.rs
+++ player/player_spec.rs
@@ -1,7 +1,16 @@
 mod player;
 
+// Stubbing
+mod elo {
+	pub mod elo {
+		pub struct Elo {
+			pub rating: f32,
+		}
+	}
+}
+
 
 #[test]
 fn create_player() {
-	let player = player::new("Andreas");
+	let player = player::new(~"Andreas");
 }
